80b6c0fbd26499af0efc2fbd1e88a7f939579a39
Prakhar005-bit <231030061@juitsolan.in>
2025-11-23 18:45:36 +0530

    script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,20 @@
-const API_URL="YOUR_API_URL_HERE";
+async function translateText() {
+    const text = document.getElementById("inputText").value;
+    const target = document.getElementById("languageSelect").value;
+
+    const API_URL = "YOUR_API_URL_HERE";  
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            text: text,
+            target_language: target
+        })
+    });
+
+    const data = await response.json();
+    document.getElementById("outputText").innerText = data.translated_text;
+}
